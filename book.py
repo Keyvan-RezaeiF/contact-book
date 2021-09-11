0859873c6240d499b38335363a0ecf5c19261e4c
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2021-09-11 17:57:21 +0430

    complete add cnotact method

--- book.py
+++ book.py
@@ -1,18 +1,50 @@
+from person import Person
+
+
 class Book:
 
     def __init__(self):
         self.people = dict()
 
 
+    def add_contact(self):
+        first_name = input("\n\tEnter contact's fisrt name : ").title()
+        last_name = input("\n\tEnter contact's last name : ").title()
+        email = input("\n\tEnter contact's email : ")
+        contact = Person(first_name, last_name, email)
+        contact.add_numbers()
+        key = contact.get_first_name() + " " + contact.get_last_name()
+        value = [contact.get_email(), contact.get_numbers()]
+        self.people[key] = value
+
+
+    def edit_contact(self):
+        pass
+
+    def search_in_book(self):
+        pass
+
+    def sort_by_sth(self):
+        pass
+
+    def delete_contact(self):
+        pass
+
+    def show_contact_book(self):
+        print()
+        print(self.people)
+
+
     def display_main_menu(self):
-        print("\t*** My contact book ***")
+        print("\n\t*** My contact book ***")
         print("---------------------------------------")
         print("\t*** 1. Add contact")
         print("\t*** 2. Edit contact")
         print("\t*** 3. Search")
         print("\t*** 4. Sort")
         print("\t*** 5. Delete contact")
-        print("\t*** 6. Exit")
+        print("\t*** 6. Show contact book")
+        print("\t*** 7. Exit")
         choice = int(input("\n\tEnter your choice : "))
 
         if choice == 1:
@@ -26,12 +58,15 @@ class Book:
         elif choice == 5:
             self.delete_contact()
         elif choice == 6:
+            self.show_contact_book()
+        elif choice == 7:
             print("\n\tSee you later!\n")
             exit()
         else:
             print("\n\tWrong input! Try again!\n")
-            self.display_main_menu()
         
+        self.display_main_menu()
+
 
 def main():
     my_contact_book = Book()
--- person.py
+++ person.py
@@ -5,3 +5,26 @@ class Person():
         self.last_name = last_name
         self.email = email
         self.numbers = list()
+
+    
+    def add_numbers(self):
+        count = int(input("\n\tHow many numbers do you want to add ? "))
+        while count > 0:
+            self.numbers.append(input("\n\tEnter the number : "))
+            count -= 1
+
+
+    def get_first_name(self):
+        return self.first_name
+
+    
+    def get_last_name(self):
+        return self.last_name
+
+    
+    def get_email(self):
+        return self.email
+
+
+    def get_numbers(self):
+        return self.numbers
